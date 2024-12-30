0c4e5233b6e387d52742a3333902ab7cdbc7cb05
babujal <dercamar1@gmail.com>
2024-12-29 20:48:19 -0500

    Deleted commented import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-// import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
